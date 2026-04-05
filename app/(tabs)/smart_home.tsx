260a840355012b1be7a4ec31a9d352bc7ec82572
duchiru <ngvit31082007@gmail.com>
2026-04-05 13:52:18 +0700

    feat: mark smart home as later development

--- app/(tabs)/smart_home.tsx
+++ app/(tabs)/smart_home.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function SmartHomePage() {
   return (
-    <View>
+    <View style={styles.view}>
       <Text>Smart Home Page</Text>
+      <Text style={{ color: "gray", fontSize: 12 }}>
+        This feature is under development.
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 4,
+  },
+});
